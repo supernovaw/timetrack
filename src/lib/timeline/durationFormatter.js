fe7966d54277e4194d43544eff1485bfafc104a4
supernovaw <ruszrj@gmail.com>
2023-01-08 18:04:35 -0800

    display formatted activity duration

---
+++ src/lib/timeline/durationFormatter.js
@@ -0,0 +1,27 @@
+// Convert duration in milliseconds into a human-readable format (e.g. "27.5 minutes")
+export default function formatDuration(millis) {
+  if (typeof millis !== "number") throw new Error("expected a number, got", millis);
+  if (millis < 0) throw new Error("negative duration: " + millis);
+
+  if (millis < 1000) return millis.toFixed(0) + " ms";
+
+  const sec = millis / 1000;
+  if (sec < 60) return plural(sec.toFixed(1), "second");
+
+  const min = sec / 60;
+  if (min < 60) return plural(min.toFixed(1), "minute");
+
+  const hr = min / 60;
+  if (hr < 24) return plural(Math.floor(hr), "hour") + " " + plural(Math.floor(min % 60), "minute");
+
+  const days = hr / 24;
+  return plural(days.toFixed(1), "day");
+}
+
+// plural(1, "day") === "1 day"
+// plural(1.2, "hour") === "1.2 hours"
+// plural("1.00", "minute") === "1 minute"
+function plural(number, singular, plural = singular + "s") {
+  if (+number === 1) return "1 " + singular;
+  return number + " " + plural;
+}
--- src/lib/timeline/renderTimelineLog.js
+++ src/lib/timeline/renderTimelineLog.js
@@ -1,6 +1,7 @@
 import { timelineLog as timelineLogStore } from "../state/index";
 import { baselineY } from "./timeline";
 import { timeToPos, line } from "./utilities";
+import formatDuration from "./durationFormatter";
 
 let timelineLogValue;
 timelineLogStore.subscribe(v => timelineLogValue = v);
@@ -8,6 +9,7 @@ timelineLogStore.subscribe(v => timelineLogValue = v);
 export default function renderTimelineLog(g, w, h, range, mouseX) {
   let hoveredActivity = null;
 
+  // Render each day
   for (const day of timelineLogValue) {
     if (day.end < range[0]) continue;
     if (day.start > range[1]) break;
@@ -17,6 +19,7 @@ export default function renderTimelineLog(g, w, h, range, mouseX) {
     g.strokeStyle = "#777";
     line(g, dayStartX, baselineY + 5.5, dayEndX, baselineY + 5.5, 3);
 
+    // Render each activity of the day
     for (const activity of day.dayLog) {
       const activityStartX = timeToPos(activity.start, range, w);
       const activityEndX = timeToPos(activity.end, range, w);
@@ -30,12 +33,14 @@ export default function renderTimelineLog(g, w, h, range, mouseX) {
     }
   }
 
+  // Render the status string for the hovered activity
   if (hoveredActivity !== null) {
     const act = hoveredActivity;
 
     const statusString = act.activityName
       + (act.activitySubcategory ? ` (${act.activitySubcategory})` : "")
-      + (act.description ? `: ${act.description}` : "");
+      + (act.description ? `: ${act.description}` : "") //   e.g. ": developing timetrack"
+      + " (" + formatDuration(act.end - act.start) + ")"; // e.g. " (46.8 minutes)"
 
     g.fillText(statusString, 5, h - 20);
   }
